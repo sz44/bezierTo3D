90e4075388e6a443173f43061a70b33a6f5799d6
sz44 <semion44@gmail.com>
2024-05-29 09:07:17 -0700

    dragging node working

--- bezier.js
+++ bezier.js
@@ -20,27 +20,40 @@ function setup() {
     strokeWeight(1);
     bezier(head.x, head.y, tail.x, tail.y, cont1.x, cont1.y, cont2.x, cont2.y);
     
-    /*
-    if (pow(mouseX - head.x, 2) + pow(mouseY - head.y, 2) <= pow(PointRad, 2) && mouseIsPressed) {
+    
+    if (pow(mouseX - head.x, 2) + pow(mouseY - head.y, 2) <= pow(PointRad, 2)) {
       push();
-      stroke(255, 0, 0);
-      strokeWeight(PointRad * 2);
-      point(200, 200);
+      stroke(200, 100, 100);
+      strokeWeight(PointRad * 2 + 5);
+      point(head.x, head.y);
       pop();
-      head.x = mouseX;
-      head.y = mouseY;
     }
-    */
+    
+  }
+  // Task: on click select a node, while mouse down node follows, on release Selected = null
+  function mousePressed() {
+    // iterate points to check if mouseover any of them
+    if (pow(mouseX - head.x, 2) + pow(mouseY - head.y, 2) <= pow(PointRad, 2)) {
+      selected = head;
+    }
+  }
+  
+  function mouseReleased() {
+    selected = null;
   }
   
   function mouseDragged() {
-    if (pow(mouseX - head.x, 2) + pow(mouseY - head.y, 2) <= pow(PointRad, 2)) {
-       head.x = mouseX;
-       head.y = mouseY;
+    if (selected === null) {
+      return;
     }
+    selected.x += movedX;
+    selected.y += movedY;
   }
   
-  const PointRad = 10;
+  const PointRad = 20;
+  
+  // make only one node selectable at a time
+  let selected = null;
   
   class Point {
     constructor(x, y) {
@@ -54,3 +67,4 @@ function setup() {
     
   let cont1 = new Point(390, 390);
   let cont2 = new Point(50, 350);
+  
